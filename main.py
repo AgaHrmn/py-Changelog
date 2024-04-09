982289206e4dd552c367cdb96a5dae3cd5eaa8f3
agaolejniq <a.olejnikk@gmail.com>
2024-04-09 22:13:05 +0200

    data processed

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from project_utensils import *
 
 if __name__ == "__main__":
-    urls = [
-        "https://drive.google.com/uc?export=download&id=1wKpSpTx89dbU3SrKt-3-DqQ62kHOFHSX",
-        "https://drive.google.com/uc?export=download&id=1oYKyW7flL53smo56W9srpFdoUuz6_42x"
-    ]
+    # urls = [
+    #     "https://drive.google.com/uc?export=download&id=1wKpSpTx89dbU3SrKt-3-DqQ62kHOFHSX",
+    #      "https://drive.google.com/uc?export=download&id=1oYKyW7flL53smo56W9srpFdoUuz6_42x"
+    # ]
 
-    if not os.path.exists("download"):
-        os.makedirs("download")
+    # if not os.path.exists("download"):
+    #     os.makedirs("download")
 
-    for i, url in enumerate(urls, start=1):
-        file_path = os.path.join("download", f"file{i}.xlsx")
-        download_file(url, file_path)
+    # for i, url in enumerate(urls, start=1):
+    #     file_path = os.path.join("download", f"file{i}.xlsx")
+    #     download_file(url, file_path)
         
+    year_2023_path = r"download\file1.xlsx"
+    # year_2024_path = r"download\file2.xlsx"
+    
+    sheets_2023 = load_data(year_2023_path)
+    elektronarzedzia_2023 = sheets_2023[1]['Elektronarzedzia']
+    ostrza_2023 = sheets_2023[2]['Ostrza']
+
+#ostrza_2023
+    sorted_column_ostrza_zastosowanie = sort_column_alphabetically(ostrza_2023, 9)
+    capitalized_nazwa = capitalize_column_values(ostrza_2023, 1)
+    capitalized_typ = capitalize_column_values(ostrza_2023, 2)
+    capitalized_material = capitalize_column_values(ostrza_2023, 7)
+    capitalized_zastosowanie = capitalize_column_values(ostrza_2023, 9)
+    print("ostrza_processed")
+    print(sorted_column_ostrza_zastosowanie)
+    print(capitalized_nazwa)
+    print(capitalized_typ)
+    print(capitalized_material)
+    print(capitalized_zastosowanie)
+
+#elektronarzedzia_2023
+    capitalized_nazwa = capitalize_column_values(elektronarzedzia_2023, 1)
+    capitalized_typ_ostrza = capitalize_column_values(elektronarzedzia_2023, 3)
+    capitalized_typ_silnika = capitalize_column_values(elektronarzedzia_2023, 5)
+    capitalized_typ_zasilania = capitalize_column_values(elektronarzedzia_2023, 6)
+    print("elektronarzedzia_processed")
+    print(capitalized_nazwa)
+    print(capitalized_typ_ostrza)
+    print(capitalized_typ_silnika)
+    print(capitalized_typ_zasilania)
+
--- project_utensils.py
+++ project_utensils.py
@@ -1,6 +1,8 @@
 import os
 import requests
-
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from lxml import etree
 
 def download_file(url, save_path):
     response = requests.get(url)
@@ -13,3 +15,60 @@ def download_file(url, save_path):
         print("Wystąpił błąd podczas operacji na pliku.")
     else:
         print(f"Plik pobrany pomyślnie.")
+
+def load_data(xlsx_file):
+    sheets_list = []
+
+    workbook = load_workbook(filename=xlsx_file)
+    sheets = workbook.sheetnames
+
+    for sheet in sheets:
+        worksheet = workbook[sheet]
+        sheet_dict = {}
+        rows = []
+
+        for row in worksheet.iter_rows(values_only=True):
+            rows.append(row)
+
+        sheet_dict[sheet]=rows
+        sheets_list.append(sheet_dict)
+
+    return sheets_list
+
+
+def save_data(data, output_file):
+    workbook = Workbook()
+
+    for sheet_dict in data:
+        for sheet_name, rows in sheet_dict.items():
+            worksheet = workbook.create_sheet(title=sheet_name)
+            for row in rows:
+                worksheet.append(row)
+
+    workbook.save(filename=output_file)
+
+def sort_column_alphabetically(rows, column_num):
+    sorted_column_values = []
+    for row in rows:
+        column_values = row[column_num]
+
+        if len(column_values) > 1:
+            column_text = str(column_values)
+            words = column_text.split('\n')
+            words.sort()
+            sorted_string = '\n'.join(words)
+            sorted_column_values.append(sorted_string)
+        else:
+            return sorted_column_values.append(column_values)
+        
+    return sorted_column_values
+
+def capitalize_column_values(rows, column_num):
+    capitalized_column_values = []
+
+    for i, row in enumerate(rows):
+        if i == 0:
+            capitalized_column_values.append(row[column_num])
+        else:
+            capitalized_column_values.append(str(row[column_num]).upper())
+    return capitalized_column_values
